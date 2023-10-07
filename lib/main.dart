104d7433f3612c73fa0b5e7b6cb31b819a507ac1
Sadman017 <sadmanrahman31@gmail.com>
2023-10-07 17:44:48 +0600

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,63 @@
 import 'package:flutter/material.dart';
 void main(){
-  runApp( MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context){
-    return  MaterialApp(
+    return  const MaterialApp(
       title: "My App",
       home: HomePage(),
     );
   }
 }
 class HomePage  extends  StatefulWidget{
+  const HomePage({super.key});
+
   @override
-  _HomePageState  createState() => _HomePageState();
+  createState() => _HomePageState();
 }
 
+
 class _HomePageState extends State<HomePage> {
+
+  String myText ="Hello World";
+  void _changeText(){
+    setState((){
+      if(myText.startsWith("H")){
+        myText ="Welcome to my app";
+      }
+      else{
+        myText  = "Hello World";
+      }
+    });
+  }
+
+  Widget bodyWidget(){
+  return Container(
+    padding: const EdgeInsets.all(8.0),
+    child: Center(child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(myText),
+        ElevatedButton(
+          onPressed: _changeText,
+          child: const Text("Click"),
+        )
+      ],
+    ),)
+  );
+}
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar:  AppBar(
         title: const Text("Home Page"),
       ),
-      body: const Center(child: Text("Hello Stateful Widget"),),
+      body: bodyWidget()
     );
   }
 }
