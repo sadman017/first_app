2606155dbac89ad773f6d3ff1bd96ce88ae5b456
Sadman017 <sadmanrahman31@gmail.com>
2023-10-13 22:07:11 +0600

    Log in App

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    return  const MaterialApp(
+    return   MaterialApp(
       title: "My App",
-      home: HomePage(),
+      home: const HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
     );
   }
 }
@@ -22,43 +25,97 @@ class HomePage  extends  StatefulWidget{
 }
 
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
 
-  String myText ="Hello World";
-  void _changeText(){
-    setState((){
-      if(myText.startsWith("H")){
-        myText ="Welcome to my app";
-      }
-      else{
-        myText  = "Hello World";
-      }
-    });
-  }
-
-  Widget bodyWidget(){
-  return Container(
-    padding: const EdgeInsets.all(8.0),
-    child: Center(
-     child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(myText),
-        ElevatedButton(
-          onPressed: _changeText,
-          child: const Text("Click"),
-        )
-      ],
-    ),)
+late AnimationController _iconAnimationController;
+late Animation<double> _iconAnimation;
+@override
+void initState(){
+  super.initState();
+  _iconAnimationController = AnimationController(
+    vsync: this,
+    duration:const Duration(milliseconds: 500)
   );
+  _iconAnimation = CurvedAnimation(parent: _iconAnimationController, curve: Curves.bounceInOut
+  );
+  _iconAnimation.addListener(()=> setState(() { 
+  }));
+  _iconAnimationController.forward();
 }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar:  AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null, 
+          ),
         title: const Text("Home Page"),
+        actions: const [
+          IconButton(onPressed: null, icon: Icon(Icons.search),
+          tooltip: null,),
+        ],
       ),
-      body: bodyWidget()
+      body: Stack(
+        fit:StackFit.expand,
+        children: <Widget>[
+           const Image(
+            image: AssetImage("assets/Tacobell.jpg"),
+            fit: BoxFit.cover,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment:MainAxisAlignment.center,
+              children: <Widget>[
+                FlutterLogo(
+                  size:_iconAnimation.value*100.0 ,
+                  ),
+                  Form(
+                    child: Theme(data: ThemeData(
+                      brightness: Brightness.dark,
+                      primarySwatch: Colors.teal,
+                       inputDecorationTheme: const InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 20.0
+                        )
+                       )
+                    ),
+                  child: Container(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(children:<Widget>[
+                    TextFormField( decoration:const InputDecoration(
+                  labelText: "Enter Email", 
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  
+                  ),
+                  TextFormField( decoration:const InputDecoration(
+                  labelText: "Enter Password", 
+                  ),
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 40.0),),
+                  MaterialButton(
+                    height: 40.0,
+                    minWidth: 60.0,
+                    color: Colors.teal,
+                    textColor: Colors.white,
+                    onPressed: ()=>{},
+                    splashColor: Colors.redAccent,
+                    child: const Text("Login"),
+                    ),
+                  ],
+                  ),
+                  ),
+                    ),
+                  )
+              ],
+            )
+        ],
+      ),
     );
   }
 }
